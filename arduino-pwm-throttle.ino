c82ce8b04b628f457a4332800b0551968f9cf172
Manuel Covas <manuel.p.covas@gmail.com>
2021-06-20 23:52:13 +0100

    Removed unused function, added comments.

--- arduino-pwm-throttle.ino
+++ arduino-pwm-throttle.ino
@@ -1,14 +1,14 @@
-#define PWM_PIN      3
-#define THROTTLE_PIN PIN_A1
-#define BRAKE_PIN    PIN_A2
+#define PWM_OUTPUT_PIN         3  // Digital pin to output the PWM signal on. 
+#define THROTTLE_PIN      PIN_A1  // Analog pin to read the throttle value from.
+#define BRAKE_PIN         PIN_A2  // Analog pin to read the brake value from.
 
-#define ADC_NEUTRAL_RANGE 10
+#define ADC_NEUTRAL_RANGE     10  // Neutral range above the neutral point.
+#define ADC_MULTISAMPLE_COUNT 16  // ADC Multisample count when taking averaged readings.
 
 
-// Multisampling to provide averaged readings
-
-#define ADC_MULTISAMPLE_COUNT 16
-
+/*
+*  ADC Multisampling function
+*/
 int analogReadMultisample(uint8_t pin) {
 
     int sum = 0;
@@ -20,35 +20,22 @@ int analogReadMultisample(uint8_t pin) {
 }
 
 
-// sprintf wraper
-
-#include <stdarg.h>
-void serial_printf(char *fmt, ... ){
-        char buf[128]; // resulting string limited to 128 chars
-        va_list args;
-        va_start (args, fmt );
-        vsnprintf(buf, 128, fmt, args);
-        va_end (args);
-        Serial.print(buf);
-}
-
-
-struct {
-    int throttle;
-    int brake;
-} adc_neutral_points;
-
+/*
+*  ADC Neutral points (read at boot)
+*/
+int throttle_neutral_point;
+int brake_neutral_point;
 
 void setup() {
     
     Serial.begin(9600);
 
-    pinMode(PWM_PIN,      OUTPUT);
+    pinMode(PWM_OUTPUT_PIN,      OUTPUT);
     pinMode(THROTTLE_PIN, INPUT);
     pinMode(BRAKE_PIN,    INPUT);
 
-    adc_neutral_points.throttle = analogReadMultisample(THROTTLE_PIN);
-    adc_neutral_points.brake    = analogReadMultisample(BRAKE_PIN);
+    throttle_neutral_point = analogReadMultisample(THROTTLE_PIN);
+    brake_neutral_point    = analogReadMultisample(BRAKE_PIN);
 }
 
 
@@ -62,19 +49,22 @@ void loop() {
     throttle_reading = analogReadMultisample(THROTTLE_PIN);
     brake_reading    = analogReadMultisample(BRAKE_PIN);
 
-    if (brake_reading > (adc_neutral_points.brake + 10)) {
+    // Ignore throttle while braking.
 
-        // Ignore throttle if braking.
-        output_pwm_duty = 128 - (brake_reading - adc_neutral_points.brake) / 8;
-
-    } else if (throttle_reading > (adc_neutral_points.throttle + 10)) {
+    if (brake_reading > (brake_neutral_point + ADC_NEUTRAL_RANGE)) {
         
-        output_pwm_duty = 128 + (throttle_reading - adc_neutral_points.throttle) / 8;
+        output_pwm_duty = 128 - (brake_reading - brake_neutral_point) / 8;
+
+    } else if (throttle_reading > (throttle_neutral_point + ADC_NEUTRAL_RANGE)) {
+        
+        output_pwm_duty = 128 + (throttle_reading - throttle_neutral_point) / 8;
 
     } else {
+
         output_pwm_duty = 128;
+        
     }
 
-    analogWrite(PWM_PIN, output_pwm_duty);
+    analogWrite(PWM_OUTPUT_PIN, output_pwm_duty);
     delay(10);
 }
